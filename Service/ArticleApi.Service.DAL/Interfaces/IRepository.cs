f44cba1848a6fa27a70c0adcc436bb47002f79ca
Clement O <clement.o@outlook.com>
2018-06-17 18:46:10 +0100

    repository interface added

---
+++ Service/ArticleApi.Service.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ArticleApi.Service.DAL.Interfaces
+{
+    public interface IRepository<TEntity, in TKey> where TEntity : class
+    {
+        TEntity Get(TKey id);
+        void Save(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
