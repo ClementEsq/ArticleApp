373e2883de78dc666f8083e4620bc0f6e579a56f
Clement O <clement.o@outlook.com>
2018-06-19 06:47:01 +0100

    Created a service to handle logging in

---
+++ Service/ArticleApi.Service.Infrastructure/Interfaces/ILogInService.cs
@@ -0,0 +1,13 @@
+﻿using ArticleApi.Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleApi.Service.Infrastructure.Interfaces
+{
+    public interface ILogInService
+    {
+        Task<GenericResponse<object>> LogIn(string email, string password);
+    }
+}
---
+++ Service/ArticleApi.Service.Infrastructure/LogInService.cs
@@ -0,0 +1,42 @@
+﻿using ArticleApi.Service.Infrastructure.Interfaces;
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using ArticleApi.Service.DTO;
+using ArticleApi.Service.DAL.Interfaces;
+
+namespace ArticleApi.Service.Infrastructure
+{
+    public class LogInService : ILogInService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public LogInService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GenericResponse<object>> LogIn(string email, string password)
+        {
+            var response = new GenericResponse<object>();
+
+            try
+            {
+                response.Status = HttpStatusCode.OK;
+                response.Message = "Success";
+
+                var user = await _userRepository.GetUserByEmail(email);
+                var isValidUser = user.Password.Equals(password);
+
+                response.Payload = isValidUser;
+            }
+            catch (Exception ex)
+            {
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = "Failed";
+            }
+
+            return response;
+        }
+    }
+}
