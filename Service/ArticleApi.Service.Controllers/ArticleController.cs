e37bb6e37a943244cede30c7d47910b2a2f849ad
Clement O <clement.o@outlook.com>
2018-06-19 06:53:11 +0100

    Article controller added

---
+++ Service/ArticleApi.Service.Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using ArticleApi.Service.DTO;
+using ArticleApi.Service.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ArticleApi.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Default")]
+    public class ArticleController
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        [HttpGet]
+        public async Task<GenericResponse<object>> GetAllTopicAreas()
+        {
+            return await _articleService.GetAllArticles();
+        }
+    }
+}
