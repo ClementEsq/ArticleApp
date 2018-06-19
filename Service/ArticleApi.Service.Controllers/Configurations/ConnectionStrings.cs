3a46ca9530b5cdfdbf9edddf788ce53aa4e13918
Clement O <clement.o@outlook.com>
2018-06-19 06:49:54 +0100

    Added a class for connection strings and ServiceExtensions.cs

---
+++ Service/ArticleApi.Service.Controllers/Configurations/ConnectionStrings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleApi.Service.Controllers.Configurations
+{
+    public class ConnectionStrings
+    {
+        public string ArticleDbConnectionString { get; set; }
+    }
+}
---
+++ Service/ArticleApi.Service.Controllers/Extensions/ServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using ArticleApi.Service.DAL;
+using ArticleApi.Service.DAL.Interfaces;
+using ArticleApi.Service.Infrastructure;
+using ArticleApi.Service.Infrastructure.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ArticleApi.Service.Controllers.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            // services.AddTransient<ITopicAreaService, TopicAreaService>();
+            RegisterDataAccessComponents(services);
+            RegisterRepositories(services);
+            RegisterInfrastructureServices(services);
+
+            return services;
+        }
+
+        private static void RegisterDataAccessComponents(IServiceCollection services)
+        {
+
+            services.AddSingleton<IDbConnection>(conn => new SqlConnection("Server=.\\SQLEXPRESS;Database=NewsArticleRepositoryDb;Integrated Security=SSPI"));
+            //services.BuildServiceProvider();
+
+            services.AddTransient<IArticleRepositoryConnection, ArticleRepositoryConnection>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IDalSession, DalSession>();
+        }
+
+        private static void RegisterRepositories(IServiceCollection services)
+        {
+
+            services.AddTransient<IArticleRepository, ArticleRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+        }
+
+        private static void RegisterInfrastructureServices(IServiceCollection services)
+        {
+
+            services.AddTransient<IArticleService, ArticleService>();
+        }
+    }
+}
