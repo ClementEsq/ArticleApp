04cf97031ad98d2be82ca854db324ac6b5950c3f
Clement O <clement.o@outlook.com>
2018-06-19 06:39:29 +0100

    Added article service

---
+++ Service/ArticleApi.Service.Infrastructure/ArticleService.cs
@@ -0,0 +1,139 @@
+﻿using ArticleApi.Service.Infrastructure.Interfaces;
+using System;
+using ArticleApi.Service.DTO;
+using ArticleApi.Service.DTO.Requests;
+using System.Threading.Tasks;
+using ArticleApi.Service.DAL.Interfaces;
+using System.Net;
+using ArticleApi.Service.Models;
+using ArticleApi.Service.DTO.Helpers;
+
+namespace ArticleApi.Service.Infrastructure
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<HttpStatusCode> DeleteArticle(int articleId)
+        {
+            var responseCode = HttpStatusCode.OK;
+
+            try
+            {
+                await _articleRepository.Delete(new Article() { ArticleId = articleId });
+            }
+            catch (Exception ex)
+            {
+                responseCode = HttpStatusCode.InternalServerError;
+            }
+
+            return responseCode;
+        }
+
+        public async Task<HttpStatusCode> PublishArticle(ArticleRequest article)
+        {
+            var responseCode = HttpStatusCode.OK;
+
+            try
+            {
+                var articleObj = DTOConverterHelper.CreateArticleObjectFromRequest(article);
+                await _articleRepository.Save(articleObj);
+            }
+            catch (Exception ex)
+            {
+                responseCode = HttpStatusCode.InternalServerError;
+            }
+
+            return responseCode;
+        }
+
+        public async Task<GenericResponse<object>> GetAllArticles()
+        {
+            var response = new GenericResponse<object>();
+
+            try
+            {
+                response.Status = HttpStatusCode.OK;
+                response.Message = "Success";
+
+                var articles = await _articleRepository.GetAllArticles();
+
+                response.Payload = articles;
+            }
+            catch (Exception ex)
+            {
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = "Failed";
+            }
+
+            return response;
+        }
+
+        public async Task<GenericResponse<object>> GetArticle(int articleId)
+        {
+            var response = new GenericResponse<object>();
+
+            try
+            {
+                response.Status = HttpStatusCode.OK;
+                response.Message = "Success";
+
+                var article = await _articleRepository.Get(articleId);
+
+                response.Payload = article;
+            }
+            catch (Exception ex)
+            {
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = "Failed";
+            }
+
+            return response;
+        }
+
+        public async Task<GenericResponse<object>> GetArticleLikes(int articleId)
+        {
+            var response = new GenericResponse<object>();
+
+            try
+            {
+                response.Status = HttpStatusCode.OK;
+                response.Message = "Success";
+
+                var article = await _articleRepository.GetAllArticleLikes(articleId);
+
+                response.Payload = article;
+            }
+            catch (Exception ex)
+            {
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = "Failed";
+            }
+
+            return response;
+        }
+
+        public async Task<HttpStatusCode> LikeArticle(int articleId, int userId)
+        {
+            var responseCode = HttpStatusCode.OK;
+
+            try
+            {
+
+                await _articleRepository.AddArticleLike(articleId, userId);
+
+            }
+            catch (Exception ex)
+            {
+                responseCode = HttpStatusCode.InternalServerError;
+            }
+
+            return responseCode;
+        }
+    }
+}
---
+++ Service/ArticleApi.Service.Infrastructure/Interfaces/IArticleService.cs
@@ -0,0 +1,17 @@
+﻿using ArticleApi.Service.DTO;
+using ArticleApi.Service.DTO.Requests;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ArticleApi.Service.Infrastructure.Interfaces
+{
+    public interface IArticleService
+    {
+        Task<HttpStatusCode> PublishArticle(ArticleRequest article);
+        Task<HttpStatusCode> DeleteArticle(int articleId);
+        Task<GenericResponse<object>> GetAllArticles();
+        Task<GenericResponse<object>> GetArticle(int articleId);
+        Task<HttpStatusCode> LikeArticle(int articleId, int userId);
+        Task<GenericResponse<object>> GetArticleLikes(int articleId);
+    }
+}
