5becaade59dbd9ae2933c59a795cb44fb95ee484
Clement O <clement.o@outlook.com>
2018-06-21 07:19:40 +0100

    Added controller for auth and registration

---
+++ Service/ArticleApi.Service.Controllers/AuthorisationController.cs
@@ -0,0 +1,30 @@
+﻿using ArticleApi.Service.DTO;
+using ArticleApi.Service.DTO.Requests;
+using ArticleApi.Service.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ArticleApi.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/auth")]
+    public class AuthorisationController : Controller
+    {
+        private readonly ILogInService _logInService;
+
+        public AuthorisationController(ILogInService logInService)
+        {
+            _logInService = logInService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<GenericResponse<bool>>> Login([FromBody]LoginRequest lr)
+        {
+
+            var response = await _logInService.LogIn(lr.Email, lr.Password);
+
+            return response;
+        }
+    }
+}
---
+++ Service/ArticleApi.Service.Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using ArticleApi.Service.DTO;
+using ArticleApi.Service.DTO.Requests;
+using ArticleApi.Service.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ArticleApi.Service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/user")]
+    public class RegistrationController : Controller
+    {
+        private readonly IRegistrationService _registrationService;
+
+        public RegistrationController(IRegistrationService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        [HttpPost]
+        [Route("signup")]
+        public async Task<ActionResult<GenericResponse<string>>> SignUp([FromBody]SignUpRequest sur)
+        {
+
+            var response = await _registrationService.SignUp(sur);
+
+            return response;
+        }
+    }
+}
