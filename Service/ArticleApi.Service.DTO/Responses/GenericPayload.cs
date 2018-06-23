52701bee46a633dc2a606998d24718a109342a4e
Clement O <clement.o@outlook.com>
2018-06-23 15:15:43 +0100

    GenericPayload class

---
+++ Service/ArticleApi.Service.DTO/Responses/GenericPayload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleApi.Service.DTO.Responses
+{
+    public class GenericPayload
+    {
+        public bool IsSuccess { get; set; }
+        public string SpecificMessage { get; set; }
+    }
+}
